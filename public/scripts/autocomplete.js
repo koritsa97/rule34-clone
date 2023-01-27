40418703606c3b1b957b1e3247abb68fa13e4bd9
koritsa97 <olia.korniv@gmail.com>
2023-01-28 00:19:41 +0200

    moved autocomplete to class, added to all pages

--- public/scripts/autocomplete.js
+++ public/scripts/autocomplete.js
@@ -1,24 +1,87 @@
-const tagsInput = document.querySelector('.tags-input');
-const tagsAutocomplete = document.querySelector('.tags-autocomplete');
+export class TagsAutocomplete {
+  constructor(rootSelector) {
+    this.root = document.querySelector(rootSelector);
+    this.input = this.root.querySelector('.form-control');
+    this.list = this.root.querySelector('.list-group');
 
-if (tagsInput && tagsAutocomplete) {
-  if (location.search !== '') {
-    const params = location.search
-      .replace('?', '')
-      .split('&')
-      .reduce((prev, property) => {
-        const [key, value] = property.split('=');
-        prev[key] = value;
-        return prev;
-      }, {});
-    const tags =
-      params.query[params.query.length - 1] === '+'
-        ? params.query.slice(0, params.query.length - 1).split('+')
-        : [params.query];
-    tagsInput.value = tags.join(' ') + ' ';
+    if (location.search !== '') {
+      this.initInputValue(location.search);
+    }
+
+    this.input.addEventListener(
+      'input',
+      this.debounce(this.handleInput.bind(this), 250)
+    );
+
+    this.input.addEventListener(
+      'blur',
+      this.debounce(this.handleInputBlur.bind(this), 1000)
+    );
+
+    this.list.addEventListener('click', this.handleListClick.bind(this));
   }
 
-  function debounce(cb, timeout) {
+  handleInputBlur() {
+    this.list.hidden = true;
+  }
+
+  handleListClick(e) {
+    const tagElem = e.target.closest('.list-group-item');
+    if (!tagElem) {
+      return;
+    }
+
+    const tag = tagElem.dataset.tag;
+    const tags = this.input.value.split(' ');
+    tags[tags.length - 1] = tag;
+    this.input.value = tags.join(' ') + ' ';
+
+    this.list.hidden = true;
+  }
+
+  handleInput(e) {
+    const inputValue = e.target.value.trim().toLowerCase();
+    if (inputValue === '') {
+      this.list.hidden = true;
+      return;
+    }
+
+    const tags = inputValue.split(' ');
+    const query = tags[tags.length - 1];
+    fetch(`/tags/autocomplete?query=${query}`)
+      .then((res) => res.json())
+      .then((tags) => this.renderTags(query, tags.slice(0, 10)));
+  }
+
+  renderTags(query, tags) {
+    this.list.hidden = false;
+    let html = '';
+    for (const tag of tags) {
+      html += this.createTag(query, tag);
+    }
+
+    this.list.innerHTML = html;
+  }
+
+  createTag(query, tag) {
+    return `<button type="button" data-tag="${
+      tag.name
+    }" class="list-group-item list-group-item-action">
+        ${tag.name.replace(query, `<mark>${query}</mark>`)}
+      </button>`;
+  }
+
+  initInputValue(searchString) {
+    const searchParams = new URLSearchParams(searchString);
+    const query = searchParams.get('query');
+    const tags =
+      query[query.length - 1] === '+'
+        ? query.slice(0, query.length - 1).split('+')
+        : [query];
+    this.input.value = tags.join(' ') + ' ';
+  }
+
+  debounce(cb, timeout) {
     let timerId;
     return (...args) => {
       if (timerId) {
@@ -28,53 +91,5 @@ if (tagsInput && tagsAutocomplete) {
         cb(...args);
       }, timeout);
     };
-  }
-
-  tagsInput.oninput = debounce((e) => {
-    const inputValue = e.target.value.trim().toLowerCase();
-    if (inputValue === '') {
-      tagsAutocomplete.hidden = true;
-      return;
-    }
-
-    const tags = inputValue.split(' ');
-    const query = tags[tags.length - 1];
-    fetch(`/tags/autocomplete?query=${query}`)
-      .then((res) => res.json())
-      .then((tags) => renderTags(query, tags));
-  }, 250);
-
-  tagsInput.onblur = debounce(() => {
-    tagsAutocomplete.hidden = true;
-  }, 100);
-
-  tagsAutocomplete.onclick = (e) => {
-    const tagElem = e.target.closest('li');
-    if (!tagElem) {
-      return;
-    }
-
-    const tag = tagElem.dataset.tag;
-    const tags = tagsInput.value.split(' ');
-    tags[tags.length - 1] = tag;
-    tagsInput.value = tags.join(' ') + ' ';
-
-    tagsAutocomplete.hidden = true;
-  };
-
-  function createTag(query, tag) {
-    return `<li data-tag="${tag.name}">
-    ${tag.name.replace(query, `<mark>${query}</mark>`)}
-  </li>`;
-  }
-
-  function renderTags(query, tags) {
-    tagsAutocomplete.hidden = false;
-    let html = '';
-    for (const tag of tags) {
-      html += createTag(query, tag);
-    }
-
-    tagsAutocomplete.innerHTML = html;
   }
 }
