2637428fb04718047935f30638b7d8601f6ffb67
koritsa97 <olia.korniv@gmail.com>
2023-01-30 19:32:32 +0200

    added tags autocomplete to upload and edit post

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,8 +1,8 @@
 const fileInput = document.querySelector('input[type="file"]');
 const previewImgElem = document.querySelector('#imagePreview');
 
-fileInput.oninput = (e) => {
+fileInput.addEventListener('input', (e) => {
   previewImgElem.src = URL.createObjectURL(e.target.files[0]);
   previewImgElem.classList.remove('d-none');
   previewImgElem.classList.add('d-block');
-};
+});
